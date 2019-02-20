7d90324deada3567f24d9fadc2a98ea7edbaed33
Brian Zick <brian.a.zick@gmail.com>
2019-02-20 16:34:03 -0500

    add C file template

---
+++ main_template.c
@@ -0,0 +1,39 @@
+/* 
+ * File:   
+ * Author: 
+ * Date:
+ * Purpose:
+ * Modified:
+ */
+/*********** COMPILER DIRECTIVES *********/
+
+// #include for textbook library header files
+#include "pic24_all.h"
+
+// #defines for handy constants 
+#define LED (_LATA0)  // LED on microstick, RA0 (pin 2)
+
+
+/*********** GLOBAL VARIABLE AND FUNCTION DEFINITIONS *******/
+
+
+    
+/********** MAIN PROGRAM ********************************/
+int main ( void )  //main function that....
+{ 
+/* Define local variables */
+
+
+/* Call configuration routines */
+	configClock();  //Sets the clock to 40MHz using FRC and PLL
+
+
+/* Initialize ports and other one-time code */
+
+
+    
+/* Main program loop */
+	while (1) {	
+		
+		}
+}
